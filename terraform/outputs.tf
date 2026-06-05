c9ef3959b066b805b35d17114e64317c286967e8
Varsha Rajashekar <161877635+VarshaRajashekar@users.noreply.github.com>
2026-06-04 22:05:49 -0500

    Fix output block formatting in outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,4 @@
 output "public_ip" {
   value = aws_instance.jenkins_server.public_ip
 }
+<img width="868" height="515" alt="Screenshot 2026-06-03 213416" src="https://github.com/user-attachments/assets/f43649a1-7f01-4b46-bfce-d4608eef95b9" />
